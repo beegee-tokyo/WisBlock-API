b97126118459436a562453f53e66a9da55b21cf7
beegee-tokyo <bernd@giesecke.tk>
2022-12-05 12:44:18 +0800

    Fix example

--- examples/WisBlock-Kit-2/app.h
+++ examples/WisBlock-Kit-2/app.h
@@ -62,6 +62,25 @@
 #define MYLOG(...)
 #endif
 #endif
+#if defined ESP32
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)                                                 \
+	if (tag)                                                            \
+		Serial.printf("[%s] ", tag);                                    \
+	Serial.printf(__VA_ARGS__);                                         \
+	Serial.printf("\n");                                                \
+	if (g_ble_uart_is_connected)                                        \
+	{                                                                   \
+		char buff[255];                                                 \
+		int len = sprintf(buff, __VA_ARGS__);                           \
+		uart_tx_characteristic->setValue((uint8_t *)buff, (size_t)len); \
+		uart_tx_characteristic->notify(true);                           \
+		delay(50);                                                      \
+	}
+#else
+#define MYLOG(...)
+#endif
+#endif
 
 /** Application function definitions */
 void setup_app(void);
